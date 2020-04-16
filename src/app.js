d8e82b66bc3f0175b520ef7c22ebba5dae7c961b
Rodrigo Rocil Manfre <rodrigodigao89@gmail.com>
2020-04-16 17:06:33 -0300

    Conceitos NodJS

---
+++ src/app.js
@@ -0,0 +1,84 @@
+const express = require("express");
+const { uuid, isUuid } = require('uuidv4');
+const cors = require("cors");
+
+// const { uuid } = require("uuidv4");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+function validarUuid(request, response, next) {
+  const { id } = request.params;
+  
+  if (!isUuid(id)) {
+    response.status(400).json({ error: 'Invalid respository ID.' });
+  }
+  return next();
+}
+
+function validarLike(request, response, next) {
+  const { likes } = request.body;
+
+  if (!likes || likes > 0) {
+    request.body.likes = 0;
+  }
+  return next();
+}
+
+const repositories = [];
+
+app.get("/repositories", (request, response) => {
+  return response.json(repositories);
+});
+
+app.post("/repositories", validarLike, (request, response) => {
+  const { title, url, techs, likes } = request.body;
+
+  const repository = { id: uuid(), title, url, techs, likes };
+  repositories.push(repository);
+
+  response.json(repository);
+});
+
+app.put("/repositories/:id", validarUuid, validarLike, (request, response) => {
+  const { id } = request.params;
+  const { title, url, techs, likes } = request.body;
+  const repositoryIndex = repositories.findIndex(r => r.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({error: 'Repository not found.'})
+  }
+
+  const repository = {id, title, url, techs, likes};
+  repositories[repositoryIndex] = repository;
+  return response.json(repository);
+});
+
+app.delete("/repositories/:id", validarUuid, (req, res) => {
+  const { id } = req.params;
+  const repositoryIndex = repositories.findIndex(r => r.id === id);
+
+  if (repositoryIndex < 0) {
+    return res.status(400).json({error: 'Repository not found.'})
+  }
+
+  repositories.splice(repositoryIndex, 1);
+  return res.status(204).send();
+});
+
+app.post("/repositories/:id/like", validarUuid, (request, response) => {
+  const { id } = request.params;
+  const repository = repositories.find(r => r.id === id);
+
+  if (!repository) {
+    return response.status(400).json({error: 'Repository not found.'})
+  }
+
+  repository.likes++;
+  return response.json({"likes": repository.likes});
+
+});
+
+module.exports = app;
